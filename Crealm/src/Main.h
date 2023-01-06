4e6d4d4d75fd2bf6a31201d1f8740933442a201d
Mattis Kjellerås <mattis@gr8.site>
2023-01-06 03:02:05 +0100

    Added functionality to main loop

---
+++ Crealm/src/Main.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <stdio.h>
+
+#pragma warning(push, 2)
+#include <Windows.h>
+#pragma warning(pop)
+
+#define GAME_NAME L"Crealm"
+
+#define TOSTRING(X) #X
+
+HWND g_hwnd;
+BOOL g_Running = FALSE;
+
+HINSTANCE GetInstance()
+{
+	return GetModuleHandle(NULL);
+}
+
+LRESULT CALLBACK MainWindowProcedure(
+	HWND	p_Hwnd,
+	UINT	p_Msg,
+	WPARAM	p_Wide,
+	LPARAM	p_Low	// low — meaing short (considering the term wide)
+);
+
+DWORD CreateMainGameWindow(void);
+
+BOOL IsGameAlreadyRunning(void);
+
+void ProcessPlayerInput(void);
+
+void RenderFrameGraphics(void);
--- Crealm/src/main.c
+++ Crealm/src/main.c
@@ -1,43 +1,43 @@
-#include <stdio.h>
+#include "Main.h"
 
-#pragma warning(push, 2)
-#include <Windows.h>
-#pragma warning(pop)
-
-HINSTANCE GetInstance()
-{
-	return GetModuleHandle(NULL);
-}
-
-HWND g_hwnd;
-
-LRESULT CALLBACK MainWindowProcedure(
-	HWND	p_Hwnd,
-	UINT	p_Msg,
-	WPARAM	p_Wide,
-	LPARAM	p_Low	// low — meaing short (considering the term wide)
-);
-
-DWORD CreateMainGameWindow();
-
-int APIENTRY WinMain(
+int APIENTRY WinMain(	// Main entrypoint for the application
 	_In_     HINSTANCE p_Instance,
 	_In_opt_ HINSTANCE p_PreviousInstance,
 	_In_     LPSTR p_CommandLine,
 	_In_     int p_ShowCommand)
 {
+
+	if (IsGameAlreadyRunning())
+	{
+		return 0;
+	}
+
 	if (CreateMainGameWindow() != ERROR_SUCCESS)
 	{
 		return 1;
 	}
 
-	MSG message;
-	ZeroMemory(&message, sizeof(message));
 
-	while (GetMessage(&message, NULL, NULL, NULL) > 0)		//	 Message / Window loop
+	MSG message = { 0 };
+	g_Running = TRUE;
+
+	while (g_Running)	// Message / Window loop
 	{
-		TranslateMessage(&message);
-		DispatchMessage(&message);
+		while (PeekMessage(&message, g_hwnd, 0, 0, PM_REMOVE))
+		{
+			TranslateMessage(&message);
+			DispatchMessage(&message);
+		}
+		
+		// process player input
+		ProcessPlayerInput();
+
+		// update game
+
+		// render game
+		RenderFrameGraphics();
+
+		Sleep(0);
 	}
 
 	return 0; 
@@ -54,6 +54,7 @@ LRESULT CALLBACK MainWindowProcedure(
 		case WM_DESTROY:
 		{
 			PostQuitMessage(0);
+			g_Running = FALSE;
 		}
 	default:
 		return DefWindowProc(p_Hwnd, p_Msg, p_Wide, p_Low);
@@ -76,7 +77,7 @@ DWORD CreateMainGameWindow()
 		windowClass.hCursor = LoadCursor(NULL, IDC_WAIT);
 		windowClass.hbrBackground = (HBRUSH)(COLOR_WINDOW);
 		windowClass.lpszMenuName = 0;
-		windowClass.lpszClassName = L"Hello Crealm";
+		windowClass.lpszClassName = GAME_NAME;
 	}
 
 	if (!(RegisterClass(&windowClass))) // register the window class
@@ -91,7 +92,7 @@ DWORD CreateMainGameWindow()
 		windowClass.lpszClassName,			// Window name
 		WS_OVERLAPPEDWINDOW,				// Styles 
 		CW_USEDEFAULT, CW_USEDEFAULT,		// Pos x, y
-		240, 120,							// Width, Height
+		768, 432,							// Width, Height
 		NULL, NULL,
 		GetInstance(),
 		NULL
@@ -106,4 +107,26 @@ DWORD CreateMainGameWindow()
 	ShowWindow(g_hwnd, SW_SHOW);
 
 	return result;
+}
+
+BOOL IsGameAlreadyRunning(void)
+{
+	HANDLE mutex = { 0 };
+	mutex = CreateMutex(NULL, GAME_NAME, L"_GameMutex");
+
+	if (GetLastError() == ERROR_ALREADY_EXISTS)
+	{
+		return 1; // true
+	}
+	return 0; // false
+}
+
+void ProcessPlayerInput(void)
+{
+
+}
+
+void RenderFrameGraphics(void)
+{
+
 }
